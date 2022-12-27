da57f2597b1d2acab60ff5607761ea0f962d180b
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2022-12-27 11:30:35 -0300

    uptade Fornecer.java to working
    Adicionando funcionalidades de fornecimento de produtos, que também é uma funcionalidade de alteração, só que de quantidade de produtos

--- src/menu/Fornecer.java
+++ src/menu/Fornecer.java
@@ -1,13 +1,39 @@
 package menu;
 
 import java.util.Scanner;
-
+import registration.Produto;
 import storage.Dados;
 
 public class Fornecer {
 
     public static void fornecimento(Dados dados, Scanner scanner) {
-        
+        String nomeProduto;
+        int quantidade = 0;
+
+        scanner.nextLine();
+        System.out.print("\nInsira o nome do produto que deseja buscar: ");
+        nomeProduto = scanner.nextLine();
+
+        boolean verification = true;
+
+        for (Produto p : dados.getProdutos()) {
+
+            if (p.getNomeProduto().equalsIgnoreCase(nomeProduto)) {
+                
+                verification = false;
+
+                System.out.println("\nProduto Encontrado.");
+                System.out.print("\nInsira a nova quantidade a repor: ");
+                quantidade = scanner.nextInt();
+
+                p.setQuantidade(quantidade);
+                
+                System.out.println("\nReposição concluída.");
+            }
+
+        }
+        if (verification) {
+            System.out.println("\nProduto Inexistente.");
+        }
     }
-    
 }
