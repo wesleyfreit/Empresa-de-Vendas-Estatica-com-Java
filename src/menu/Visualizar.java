c782f3bb665dfaa19b4d6d9a9710c9d388cdb0f6
RICHARD WESLEY BARBOSA FREITAS <wesleyrichard30@gmail.com>
2022-12-27 08:55:52 -0300

    update visualizar.java to working
    adicionando funções de visualização de lista

--- src/menu/Visualizar.java
+++ src/menu/Visualizar.java
@@ -1,13 +1,75 @@
 package menu;
 
 import java.util.Scanner;
-
+import registration.Produto;
 import storage.Dados;
 
 public class Visualizar {
 
     public static void ver(Dados dados, Scanner scanner) {
-        
+        boolean run = true;
+
+        do {
+            System.out.println("\n------------------------\n| PRODUTOS CADASTRADOS |\n------------------------");
+            System.out.print("1 - Ver Produtos Estocados\n2 - Ver Valores\n3 - Voltar\n\nDigite: ");
+            switch (scanner.nextInt()) {
+                case 1:
+                    if (verQuantidade(dados))
+                        System.out.println("\nNão há produtos cadastrados.");
+                    break;
+                case 2:
+                    if (verValor(dados))
+                        System.out.println("\nNão há produtos cadastrados.");
+                    break;
+                case 3:
+                    run = false;
+                    break;
+                default:
+                    System.out.println("\nInsira uma opção válida");
+                    break;
+            }
+        } while (run);
+    }
+
+    public static boolean verQuantidade(Dados dados) {
+        boolean ilegivel = true;
+
+        if (dados.sizeArray() > 0) {
+            ilegivel = false;
+
+            System.out.println("\n------------------------------------");
+            System.out.println("| Produto -> Quantidade");
+            System.out.println("------------------------------------");
+
+            for (Produto p : dados.getProdutos()) {
+                System.out.print("| " + p.getNomeProduto());
+                System.out.println(" -> " + p.getQuantidade());
+            }
+            System.out.println("------------------------------------");
+            System.out.println("------------------------------------");
+        }
+
+        return ilegivel;
+    }
+
+    public static boolean verValor(Dados dados) {
+        boolean ilegivel = true;
+        if (dados.sizeArray() > 0) {
+            ilegivel = false;
+
+            System.out.println("\n------------------------------------");
+            System.out.println("| Produto -> Valor");
+            System.out.println("------------------------------------");
+
+            for (Produto p : dados.getProdutos()) {
+                System.out.print("| " + p.getNomeProduto());
+                System.out.println(" -> " + p.getValor());
+            }
+            System.out.println("------------------------------------");
+            System.out.println("------------------------------------");
+        }
+        return ilegivel;
+
     }
     
 }
